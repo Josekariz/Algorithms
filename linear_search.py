f7c51f5ed2ef5b2d3824504d420a6570097132c5
Joseph Kariuki Macharia <sejokarizz@gmail.com>
2023-09-12 07:41:05 +0000

    set target var to val from user input

--- linear_search.py
+++ linear_search.py
@@ -5,15 +5,16 @@ def linear_search(arr, target):
         if arr[i] == target:
             # If found, return the index where the target is located.
             return i
-    
+
     # If the target is not found in the list, return -1 to indicate it wasn't found.
     return -1
+
 
 # Define the list outside of the function.
 my_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 
-# Define the target outside of the function.
-target = 9
+# Get input target outside of the function.
+target = (input("Enter target No: "))
 
 # Call the linear_search function.
 result = linear_search(my_list, target)
